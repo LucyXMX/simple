8e3fc6d60a99e7df98f5620730e0189912a27421
Xing, Mu Xi <lucy.xing@gatech.edu>
2021-08-07 20:41:23 -0400

    fail test

--- test/test_simple.py
+++ test/test_simple.py
@@ -5,4 +5,4 @@ class TestSimple(unittest.TestCase):
 
     def test_add(self):
         simple = src.simple.Simple()
-        self.assertEqual(2.0,simple.add(1,1))
+        self.assertEqual(2.0,simple.add(1,2))
